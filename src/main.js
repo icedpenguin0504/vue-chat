94f209bb9a5c2bbf6c3d317e75286804760f479d
swimpenguin <swimpenguin0504@gmail.com>
2019-11-02 18:48:39 +0900

    [add] Introduce firebase

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
+import firebase from 'firebase'
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBWS0Z73oNJk8fMDWf_Uh3wXaksYLR8UlM",
+  authDomain: "vue-chat-7d692.firebaseapp.com",
+  databaseURL: "https://vue-chat-7d692.firebaseio.com",
+  projectId: "vue-chat-7d692",
+  storageBucket: "vue-chat-7d692.appspot.com",
+  messagingSenderId: "10848147312",
+  appId: "1:10848147312:web:fcd8eff5dd874ccd6061b5",
+  measurementId: "G-H55V2NESKG"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   render: h => h(App),
