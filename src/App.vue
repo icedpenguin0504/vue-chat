3c16e2f53d57f5b32cdbc5910559797356b23ed1
swimpenguin <swimpenguin0504@gmail.com>
2019-11-06 18:15:35 +0900

    [add] Add feature to chat

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>Chat App with Vue.js</header>
+    <div class="main">
+      <Chat
+        v-for="(msg, index) in chat"
+        :message="msg"
+        :key="index"/>
+    </div>
+    <form action="">
+      <textarea
+        placeholder="Press Enter to send."
+        @keydown.enter.exact.prevent="doSend"
+        v-model="text"></textarea>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Chat from './components/Chat.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Chat
+  },
+  data() {
+    return {
+      chat: [],
+      text: ''
+    }
+  },
+  methods: {
+    doSend() {
+      const text = this.text;
+      this.chat.push(text);
+      this.text = '';
+      this.scrollToBottom();
+    },
+    scrollToBottom() {
+      // scrolls after DOM is inserted
+      this.$nextTick(() => {
+        window.scrollTo(0, document.body.clientHeight);
+      });
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+$base-color: #1BAF85;
+$width: 600px;
+$form-height: 90px;
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+header {
+  background-color: $base-color;
+  color: white;
+  height: 40px;
+  width: 100%;
+  line-height: 40px;
+  padding-left: 40px;
+  position: fixed;
+  top: 0;
+
+}
+
+.main {
+  width: $width;
+  margin: 0 auto;
+  margin-bottom: 100px;
+  margin-top: 80px;
+}
+
+form {
+  height: $form-height;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  background-color: white;
+
+  textarea {
+    display: inline-block;
+    width: $width;
+    height: calc(100% - 10px);
+    resize: none;
+    outline: none;
+    font-size: 16px;
+  }
+}
+
 </style>
---
+++ src/components/Chat.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="list">
+    <div class="baloon">{{ $props.message }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'chat',
+    props: {
+      'message': {
+        type: String
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+$baloon-color: lightgray;
+
+.list {
+  margin-top: 10px;
+}
+
+.baloon {
+  display: inline-block;
+  background-color: $baloon-color;
+  border-radius: 10px;
+  padding: 10px;
+  // reflect line breaks
+  white-space: pre-wrap;
+  word-wrap: break-word;
+}
+</style>
