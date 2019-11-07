ac75242829503c0645d91689c2ac4f2c43be068d
swimpenguin <swimpenguin0504@gmail.com>
2019-11-07 17:09:04 +0900

    [add] Add authentication with twitter

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,38 @@
 <template>
   <div id="app">
-    <header>Chat App with Vue.js</header>
+    <header>
+      <div class="header-left">
+        <p>Chat App with Vue.js</p>
+      </div>
+      <div v-if="user.uid" class="header-right">
+        <p>{{ user.displayName }}</p>
+        <button @click="doLogout">Logout</button>
+      </div>
+      <div v-else class="header-right">
+        <button @click="doLogin">Login</button>
+      </div>
+    </header>
     <div class="main">
       <Chat
-        v-for="(msg, index) in chat"
-        :message="msg"
-        :key="index"/>
+        v-for="{key, name, image, message } in chat"
+        :name="name"
+        :image="image"
+        :message="message"
+        :key="key"/>
     </div>
     <form action="">
       <textarea
         placeholder="Press Enter to send."
         @keydown.enter.exact.prevent="doSend"
-        v-model="text"></textarea>
+        v-model="text"
+        :disabled="!user.uid"></textarea>
     </form>
   </div>
 </template>
 
 <script>
 import Chat from './components/Chat.vue';
+import firebase from 'firebase';
 
 export default {
   name: 'app',
@@ -27,21 +42,56 @@ export default {
   data() {
     return {
       chat: [],
-      text: ''
+      text: '',
+      user: {}
     }
+  },
+  created() {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.user = user ? user : {};
+      const ref_message = firebase.database().ref('message');
+      if (user) {
+        this.chat = [];
+        ref_message.limitToLast(10).on('child_added', this.childAdded);
+      } else {
+        ref_message.limitToLast(10).off('child_added', this.childAdded);
+      }
+    })
   },
   methods: {
     doSend() {
-      const text = this.text;
-      this.chat.push(text);
-      this.text = '';
-      this.scrollToBottom();
+      if (this.user.uid && this.text.length) {
+        firebase.database().ref('message').push({
+          message: this.text,
+          name: this.user.displayName,
+          image: this.user.photoURL
+        }, () => {
+          this.text = '';
+        })
+      }
     },
     scrollToBottom() {
       // scrolls after DOM is inserted
       this.$nextTick(() => {
         window.scrollTo(0, document.body.clientHeight);
       });
+    },
+    doLogin() {
+      const provider = new firebase.auth.TwitterAuthProvider();
+      firebase.auth().signInWithPopup(provider);
+    },
+    doLogout() {
+      firebase.auth().signOut();
+    },
+    childAdded(snap) {
+      const message = snap.val();
+      this.chat.push({
+        key: snap.key,
+        name: message.name,
+        image: message.image,
+        message: message.message
+      });
+      this.scrollToBottom();
     }
   }
 }
@@ -62,10 +112,43 @@ header {
   height: 40px;
   width: 100%;
   line-height: 40px;
-  padding-left: 40px;
   position: fixed;
   top: 0;
 
+  .header-left {
+    width: 50%;
+    display: inline-block;
+
+    p {
+      margin: 0;
+      margin-left: 40px;
+    }
+  }
+
+  .header-right {
+    width: 50%;
+    display: inline-block;
+    text-align: right;
+
+    p {
+      margin: 0;
+      display: inline-block;
+      font-size: 14px;
+    }
+
+    button {
+      display: inline-block;
+      margin-right: 40px;
+      width: 80px;
+      height: 30px;
+      border: 1px solid white;
+      border-radius: 3px;
+      background-color: $base-color;
+      color: white;
+      font-size: 16px;
+      outline: none;
+    }
+  }
 }
 
 .main {
--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="list">
-    <div class="baloon">{{ $props.message }}</div>
+    <div class="image">
+      <img :src="$props.image" width="40" height="40">
+    </div>
+    <div class="chat">
+      <p class="name">{{ $props.name }}</p>
+      <div class="baloon">{{ $props.message }}</div>
+    </div>
+
   </div>
 </template>
 
 <script>
   export default {
     name: 'chat',
-    props: {
-      'message': {
-        type: String
-      }
-    }
+    props: ['name', 'image', 'message']
   }
 </script>
 
@@ -22,13 +25,47 @@ $baloon-color: lightgray;
   margin-top: 10px;
 }
 
+.image {
+  display: inline-block;
+
+  > img {
+    border-radius: 50%;
+  }
+}
+
+.chat {
+  display: inline-block;
+  vertical-align: top;
+  margin: 0;
+  margin-left: 20px;
+}
+
+.name {
+  color: gray;
+  font-size: 12px;
+  margin: 0;
+}
+
 .baloon {
   display: inline-block;
   background-color: $baloon-color;
   border-radius: 10px;
   padding: 10px;
+  position: relative;
   // reflect line breaks
   white-space: pre-wrap;
   word-wrap: break-word;
+}
+
+.baloon::after {
+  content: "";
+  display: inline-block;
+  position: absolute;
+  top: 3px;
+  left: -19px;
+  border: 8px solid transparent;
+  border-right: 18px solid $baloon-color;
+  -webkit-transform: rotate(35deg);
+  transform: rotate(35deg);
 }
 </style>
